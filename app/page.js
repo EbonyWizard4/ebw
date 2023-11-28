403cc90d1b8bb9373a33c566558ac5b1e12bf165
EbonyWizard4 <antonio.jhone@hotmail.com>
2023-11-28 20:26:18 -0300

    Alter page of portifolio

--- app/page.js
+++ app/page.js
@@ -1,20 +1,13 @@
-import Heder from "./components/Heder/Heder";
-import Card from "./components/card/Card";
+import Heder from "./components/Heder/Heder"
+import Card from "./components/card/Card"
+import Banner from "./components/Banner/Banner"
 
 export default function Page() {
     return (
         <main>
-            <Heder />
-            <div className='corpo'>
-                <h1>Bem vindo ao Meu Portifólio</h1>
-                <div className='lista'></div>
-                <Card 
-                    Titulo='Stock Portfolio'
-                    Descricao='Gerador de Carteira de Investimento'
-                    html_url='https://github.com/EbonyWizard4/Stock_Portfolio'
-                />
-
-            </div>
+            <Banner>
+                
+            </Banner>
         </main>
 
     )
